41a473d13da068ae20f8303ea29f0bbe10f9587c
jiyaoxiong <88077941+jiyaoxiong@users.noreply.github.com>
2026-09-04 22:33:52 +0800

    Bump C16 YouTube to v2.1.40039

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.android.gallery3d"
         minSdk = 28
         targetSdk = 35
-        versionCode = 40038
-        versionName = "2.0.40038"
+        versionCode = 40039
+        versionName = "2.1.40039"
     }
 
     buildTypes {
