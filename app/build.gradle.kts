500d46a71a3d2d9df5a3ac0fc67f9c773906140b
jiyaoxiong <88077941+jiyaoxiong@users.noreply.github.com>
2026-09-05 05:46:58 +0800

    Bump C16 YouTube to v4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.android.gallery3d"
         minSdk = 28
         targetSdk = 35
-        versionCode = 40063
-        versionName = "4.6.40063"
+        versionCode = 40064
+        versionName = "4.7.40064"
     }
 
     buildTypes {
