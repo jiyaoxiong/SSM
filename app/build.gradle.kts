285cdafa340b87a81f42123c1881f1b218d3224e
jiyaoxiong <88077941+jiyaoxiong@users.noreply.github.com>
2026-09-04 21:39:49 +0800

    Bump C16 Video to v1.4.40036

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.android.gallery3d"
         minSdk = 28
         targetSdk = 35
-        versionCode = 40035
-        versionName = "1.3.40035"
+        versionCode = 40036
+        versionName = "1.4.40036"
     }
 
     buildTypes {
